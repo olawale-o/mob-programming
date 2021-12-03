8d822d7a510fa2c0600922e4d475a9bb8cb7da0f
olawale-o <omoogunolawale@gmail.com>
2021-12-03 08:17:23 +0100

    Update README.md file

--- descending_order.rb
+++ descending_order.rb
@@ -1,7 +1,8 @@
-def descending_order()
+def descending_order(n)
   number = n.to_s
   return number.to_i if number.size < 2
+
   arr = number.split('')
-  
+
   arr.sort! { |a, b| b <=> a }.join.to_i
 end
